bb239eb8808132ef43b157201297e26366229ed5
Dima <dima@zaharov.info>
2015-11-23 21:53:09 +0200

    Common constants and ENUMs

---
+++ src/main/java/lv/div/locator/commons/conf/Const.java
@@ -0,0 +1,31 @@
+package lv.div.locator.commons.conf;
+
+/**
+ * Common constants
+ */
+public class Const {
+
+    public static final String WIFI_VALUES_SEPARATOR = ";;";
+    public static final String WIFI_NAME_SEPARATOR = "::";
+    public static final String DEVICE_ID_HTTP_PARAMETER = "device";
+    public static final String TEXT_OUTPUT_HTTP_PARAMETER = "text";
+    public static final String DEFAULT_SERVLET_ANSWER = "OK";
+
+//    Signal receiver:
+    public static final String WIFI_DATA_FIELD = "field3";
+    public static final String GPS_LATITUDE_FIELD = "field1";
+    public static final String GPS_LONGITUDE_FIELD = "field2";
+    public static final String BATTERY_LEVEL_FIELD = "field6";
+    public static final String SAFE_ZONE_FIELD = "field5";
+    public static final String ACCURACY_FIELD = "field4";
+    public static final String SPEED_FIELD = "field7";
+    public static final String DEVICEID_FIELD = "field8";
+
+    public static final String DEVICE_ID_PARAMETER_NAME = "deviceId";
+    public static final String KEY_PARAMETER_NAME = "key";
+    public static final String TIME_VALUE_FORMAT = "%02d";
+    public static final String ZERO_VALUE = "0";
+    public static final long ZERO_LONG_VALUE = 0L;
+
+
+}
