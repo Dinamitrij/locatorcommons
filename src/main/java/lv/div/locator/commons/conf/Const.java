21cc4c27817d2f6dc77f5a44ccd8378c409a8c86
Dima <dima@zaharov.info>
2015-11-23 21:56:02 +0200

    Common constants and ENUMs

--- src/main/java/lv/div/locator/commons/conf/Const.java
+++ src/main/java/lv/div/locator/commons/conf/Const.java
@@ -11,7 +11,7 @@ public class Const {
     public static final String TEXT_OUTPUT_HTTP_PARAMETER = "text";
     public static final String DEFAULT_SERVLET_ANSWER = "OK";
 
-//    Signal receiver:
+    //    Signal receiver:
     public static final String WIFI_DATA_FIELD = "field3";
     public static final String GPS_LATITUDE_FIELD = "field1";
     public static final String GPS_LONGITUDE_FIELD = "field2";
@@ -27,5 +27,12 @@ public class Const {
     public static final String ZERO_VALUE = "0";
     public static final long ZERO_LONG_VALUE = 0L;
 
+    public static final String TIME_24H_FORMAT = "HH:mm:ss";
+    public static final String DATE_FORMAT = "dd-MM";
+    public static final String UTF8_ENCODING = "utf-8";
+    public static final String EMPTY = "";
+    public static final String SPACE = " ";
+    public static final String TRUE_FLAG = "1";
+    public static final String ZERO_COORDINATE = "0.0";
 
 }
