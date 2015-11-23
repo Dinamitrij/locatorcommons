adfdd8c678e830689ab130c87f59e4ea103dc2e4
worldwar2 <worldwar2@div.lv>
2015-11-23 15:20:56 +0200

    1st util

---
+++ src/main/java/lv/div/locator/commons/utils/TimeUtils.java
@@ -0,0 +1,14 @@
+package lv.div.locator.commons.utils;
+
+import java.util.Date;
+
+/**
+ * Common time manipulation utils
+ */
+public class TimeUtils {
+
+    public Date getCurrentDate() {
+        return new Date();
+    }
+
+}
